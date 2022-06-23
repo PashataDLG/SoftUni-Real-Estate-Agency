23a7d2819ec572958841f1458677df364992ab5f
Angel Pashov <86370508+PashataDLG@users.noreply.github.com>
2022-06-24 02:59:48 +0300

    Housing validators added

--- src/models/Housing.js
+++ src/models/Housing.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const housingSchema = new mongoose.Schema({
     houseName: {
         type: String,
-        require: true
+        require: [true, 'House name is required'],
+        minLength: [6, 'The house name must be at least 6 characters long'],
     },
     type: {
         type: String,
@@ -12,11 +13,18 @@ const housingSchema = new mongoose.Schema({
     },
     year: {
         type: Number,
-        require: true
+        require: true,
+        validate: {
+            validator: function(year){
+                return year >= 1850 && year <= 2021
+            },
+            	message: 'The year must be between 1850 and 2021'
+        }
     },
     city: {
         type: String,
-        require: true
+        require: true,
+        minLength: [4, 'The city name must be at least 4 characters long']
     },
     homeImg: {
         type: String,
@@ -28,11 +36,18 @@ const housingSchema = new mongoose.Schema({
     },
     description: {
         type: String,
-        require: true
+        require: true,
+        maxLength: [60, 'The description must be no more than 60 characters long']
     },
     pieces: {
         type: Number,
-        require: true
+        require: true,
+        validate: {
+            validator: function(pieces){
+                return pieces >= 0 && pieces <= 10
+            },
+            	message: 'The Available Pieces should be positive number (from 0 to 10)'
+        }
     },
     rentedHome: [{
         type: mongoose.Types.ObjectId,
