999032423182195add669bfdb7dd96d75970dd34
Angel Pashov <86370508+PashataDLG@users.noreply.github.com>
2022-06-23 22:57:06 +0300

    Render dynamic details page (not done yet), and edit functionallity added

--- src/controllers/houseController.js
+++ src/controllers/houseController.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 
 const houseService = require('../services/houseService');
 
-router.get('/create', function (req, res){
+router.get('/create', function (req, res) {
     res.render('house/create');
 });
 
-router.post('/create', async function (req, res){
+router.post('/create', async function (req, res) {
     let houseData = req.body;
 
     houseData.owner = req.user._id;
@@ -17,6 +17,41 @@ router.post('/create', async function (req, res){
     } catch (err) {
         res.status(401).render('house/create', { error: err.message });
     }
+});
+
+router.get('/for-rent', async function (req, res) {
+    const availableHouses = await houseService.getAll().lean();
+
+    res.render('house/for-rent', { availableHouses });
+});
+
+router.get('/:houseId/details', async function (req, res) {
+    const house = await houseService.getById(req.params.houseId).lean();
+
+    const isOwner = house.owner == req.user?._id;
+
+    res.render('house/details', { house, isOwner });
+});
+
+router.get('/:houseId/edit', async function (req, res) {
+    const houseData = await houseService.getById(req.params.houseId).lean();
+
+    res.render('house/edit', { houseData });
+});
+
+router.post('/:houseId/edit', async function (req, res) {
+    let houseData = req.body;
+
+    houseData.owner = req.user._id;
+
+    try {
+        await houseService.updateHouse(req.params.houseId, houseData);
+
+        res.redirect(`/house/${req.params.houseId}/details`);
+    } catch (err) {
+        res.status(401).render('house/edit', { houseData, error: err.message });
+    }
+
 });
 
 module.exports = router;
--- src/services/houseService.js
+++ src/services/houseService.js
@@ -1,3 +1,9 @@
 const Housing = require('../models/Housing');
 
 exports.create = (houseData) => Housing.create(houseData);
+
+exports.getAll = () => Housing.find();
+
+exports.getById = (houseId) => Housing.findOne({ _id: houseId });
+
+exports.updateHouse = (houseId, houseData) => Housing.findByIdAndUpdate(houseId, houseData);
